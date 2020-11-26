62cc58eb4773d2adaa801914b3a4ff77a1d74204
turbopetrov <turbopetrov@yandex.ru>
2020-11-27 02:41:17 +0700

    we can get a coins

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-console.log('1');
+import $ from 'jquery';
+
+const $getCoinsBtn = $('#getCoinsBtn');
+const $coinsCheckbox = $('#coinsCheckbox');
+const $coin = $('.jsCoin');
+const $coinsQuantity = ('#coinsQuantity');
+
+function getFiveCoins(){    
+    for (let i=0; i<5; i++){
+        $coin.clone(true).appendTo('.coins-block__coins');        
+    }    
+}
+function getOneCoins(){
+    $coin.clone(true).appendTo('.coins-block__coins')
+}
+
+
+//----------------------------scripts------------------------------//
+
+$getCoinsBtn.on('click', ()=>{
+    ($coinsCheckbox.is(':checked'))? 
+    getFiveCoins():getOneCoins();     
+})
