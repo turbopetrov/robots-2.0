75e9224e9a44b03cc0fb3be610ad85c48ccb2ed3
turbopetrov <turbopetrov@yandex.ru>
2020-11-27 11:38:25 +0700

    js for wallet-section is ready

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,39 @@ import $ from 'jquery';
 const $getCoinsBtn = $('#getCoinsBtn');
 const $coinsCheckbox = $('#coinsCheckbox');
 const $coin = $('.jsCoin');
-const $coinsQuantity = ('#coinsQuantity');
+let coinsQuantity = 1;
+let regexp1 =/(11|12|13|14|0|5|6|7|8|9)$/;
+let regexp2 =/(2|3|4)$/;
+let regexp3 =/1$/;
 
-function getFiveCoins(){    
-    for (let i=0; i<5; i++){
-        $coin.clone(true).appendTo('.coins-block__coins');        
-    }    
+function walletMessage(){
+    if(regexp1.test(coinsQuantity)){
+        return ' biorobo монет'
+    }
+    else if(regexp2.test(coinsQuantity)){
+        return ' biorobo монеты'
+    }
+    else return ' biorobo монета'
+}
+function getFiveCoins(){  
+    if((coinsQuantity+5)>100){
+        alert('Больше в кошелек не влезет');
+    }
+    else{  
+        for (let i=0; i<5; i++){
+            $coin.clone(true).appendTo('.coins-block__coins');        
+        } 
+    coinsQuantity+=5;
+    }  
 }
 function getOneCoins(){
-    $coin.clone(true).appendTo('.coins-block__coins')
+    if((coinsQuantity+1)>100){
+        alert('Больше в кошелек не влезет')
+    }
+    else{
+        $coin.clone(true).appendTo('.coins-block__coins')
+        coinsQuantity++;
+    }    
 }
 
 
@@ -19,5 +43,6 @@ function getOneCoins(){
 
 $getCoinsBtn.on('click', ()=>{
     ($coinsCheckbox.is(':checked'))? 
-    getFiveCoins():getOneCoins();     
+    getFiveCoins():getOneCoins();
+    $('#coinsQuantity').text(coinsQuantity+walletMessage());     
 })
